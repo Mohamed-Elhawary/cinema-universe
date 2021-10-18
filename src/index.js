a7971ce29874b132209c0c8ea4d6a812d86965e7
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2021-10-18 05:21:45 +0200

    :art: update: Handled the Dark Mode Mechanism using the styled-components theming

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-	<Provider store={store}>
+
 			<React.StrictMode>
     			<App />
   			</React.StrictMode>
-	</Provider>
   , document.getElementById('root')
 );
