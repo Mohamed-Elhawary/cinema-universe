8cfa8ef0c6db3554a5ea1c13b7d916c014e4081f
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2021-10-18 16:45:23 +0200

    :lock:  update: Added the Auth environment

---
+++ src/utils/helpers.js
@@ -0,0 +1,17 @@
+import Cookies from 'js-cookie';
+
+export const getCookies = () => {
+    
+    let userName = Cookies.get("userName") || null;
+
+    let password = Cookies.get("password") || null;
+       
+    return {userName, password};
+}
+
+export const removeCookies = () => {
+    
+    Cookies.remove("userName");
+
+    Cookies.remove("password");
+}
---
+++ src/utils/use-auth.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState, useContext, createContext } from "react";
+import Cookies from 'js-cookie';
+import { getCookies, removeCookies } from "./helpers";
+
+const authContext = createContext();
+
+export function ProvideAuth({children}) {
+  
+    const auth = useProvideAuth();
+  
+    return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
+
+export const useAuth = () => {
+
+    return useContext(authContext);
+};
+
+function useProvideAuth() {
+
+  const [user, setUser] = useState(null);
+
+  const [showMainLoader, setShowMainLoader] = useState(true);
+  
+
+  const login = (userName, password, callback) => {
+
+    Cookies.set("userName", userName);
+
+    Cookies.set("password", password);
+    
+    setTimeout(() => {
+        
+        setUser(userName);
+
+        callback({status: "Successed", statusCode: 200});
+
+    }, 300);
+};
+
+
+const logout = (callback) => {
+
+    removeCookies();
+
+    setUser(null);
+
+    callback();
+};
+
+
+useEffect(() => {
+
+    setShowMainLoader(true);
+
+    const cookies = getCookies();
+
+    setTimeout(() => {
+
+        if (cookies['userName'] && cookies['password']) setUser(cookies['userName']);
+        
+        else {
+
+            removeCookies();
+
+            setUser(null); 
+         
+        }
+
+        setShowMainLoader(false);
+
+    }, 300);
+
+}, []);
+
+    return {
+        user,
+        showMainLoader,
+        login,
+        logout
+    };
+}
