77cdebe14f2a3716feaa7f0177403958a6351a12
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2021-10-18 05:21:45 +0200

    :art: update: Handled the Dark Mode Mechanism using the styled-components theming

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./GlobalStyles";
+import { lightTheme, darkTheme } from "./Themes"
 
-class App extends Component {
+const App = () => {
 
-	render() {
-		return (
+	const [theme, setTheme] = useState('light');
+  	
+	const themeToggler = () => theme === 'light' ? setTheme('dark') : setTheme('light');
+
+	return (
+		<ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+			<GlobalStyles />
 			<div className="App">
-				
+				<button onClick={themeToggler}>Switch Theme</button>
+
+				<section>
+                  <h2 className="section-title">Head 2</h2>
+                  <div>This is Div</div>
+                  <hr />
+                </section>
 			</div>
-		);	
-	}
+		</ThemeProvider>
+	);	
 }
 
 export default App;
---
+++ src/Themes.js
@@ -0,0 +1,22 @@
+/*
+    Colors DB:-
+    ------------
+
+    color_1: '#001E3C',
+    color_2: '#071A2F',
+    color_3: '#132F4C',
+    color_4: '#173A5E',
+    color_5: '#0070E0',
+    color_6: '#0059B2',
+    color_7: '#AAB4BE',
+    color_8: '#fff'
+*/
+
+export const lightTheme = {
+    body_bg: "#fff",
+
+}
+
+export const darkTheme = {
+    body_bg: '#001E3C'
+}
