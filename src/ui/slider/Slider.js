02a0656e7529251d9968905f685432efaceb8284
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2021-10-19 21:52:47 +0200

    :rocket: update: Added new UI component [Slider] Component

---
+++ src/ui/slider/Slider.js
@@ -0,0 +1,67 @@
+import chevronForward from 'assets/images/arrow-right.svg';
+import chevronBack from 'assets/images/arrow-left.svg';
+import CustomizedSlider from "./SliderStyle";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Slider = ({isPosterSlider, children}) => {
+
+    const settings = {
+		arrows: !isPosterSlider,
+		autoplay: isPosterSlider,
+		pauseOnHover: isPosterSlider,
+		centerMode: true,
+		centerPadding: "120px",
+        slidesToShow: isPosterSlider ? 1 : 5,
+        slidesToScroll: 1,
+        dots: false,
+        infinite: true,
+        speed: 500,
+		autoplaySpeed: 6000,
+        responsive: [
+            {
+				breakpoint: 1300,
+				settings: {
+					centerPadding: "100px",
+				}
+            },
+            {
+				breakpoint: 1190,
+				settings: {
+					slidesToShow: isPosterSlider ? 1 : 4,
+					centerPadding: "90px",
+				}
+            },
+			{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: isPosterSlider ? 1 : 3,
+					centerPadding: "80px",
+				}
+            },
+            {
+				breakpoint: 767,
+				settings: {
+					slidesToShow: isPosterSlider ? 1 : 2,
+					centerPadding: "70px",
+				}
+            },
+            {
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1,
+					infinite: true,
+					centerMode: true,
+					centerPadding: "60px"
+				}
+            }
+        ],
+        nextArrow: <img src={chevronForward} alt="next_arrow" />,
+        prevArrow: <img src={chevronBack} alt="prev_arrow" />
+    }
+
+
+    return <CustomizedSlider {...settings}>{children}</CustomizedSlider>;
+}
+
+export default Slider;
