abb730559e100da89aa849156e1767df66d05e0b
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2021-10-20 23:31:31 +0200

    :recycle: refactor: Integrated the MovieModal with the MovieModalSlider

--- src/components/moviemodal/MovieModal.js
+++ src/components/moviemodal/MovieModal.js
@@ -2,9 +2,10 @@ import YouTube from 'react-youtube';
 import { useState, useEffect } from "react";
 import { connect } from 'react-redux';
 import { closeMovieModal } from "actions";
-import { img_300, img_780, unavailableLandscape, unavailablePicture } from "config";
+import { img_780, unavailableLandscape } from "config";
 import { fetchingMovieData } from "services";
 import { Modal } from "ui";
+import { MovieModalSlider } from "components";
 
 const MovieModal = ({ movieModalID, movieModalOpen, hideMovieModal }) => {
 
@@ -58,28 +59,8 @@ const MovieModal = ({ movieModalID, movieModalOpen, hideMovieModal }) => {
                 ))}
             </div>
             <div className="overview">{overview}</div>
-            <div className="cast d-flex flex-wrap">
-                {credits.cast.map(person => (
-                    <div className="person-box">
-                        <div className="person-photo">
-                            <img className="w-100 h-100" src={person.profile_path ? img_300 + person.profile_path : unavailablePicture} alt="person_photo" />
-                        </div>
-                        <h6 className="person-name">{person.name}</h6>
-                        <span className="person-character">{person.character}</span>
-                    </div>
-                ))}
-            </div>
-            <div className="crew d-flex flex-wrap">
-                {credits.crew.map(person => (
-                    <div className="person-box">
-                        <div className="person-photo">
-                            <img className="w-100 h-100" src={person.profile_path ? img_300 + person.profile_path : unavailablePicture} alt="person_photo" />
-                        </div>
-                        <h6 className="person-name">{person.name}</h6>
-                        <span className="person-character">{person.department}</span>
-                    </div>
-                ))}
-            </div>
+            <MovieModalSlider isCastSlider contentArray={credits.cast} />
+            <MovieModalSlider contentArray={credits.crew} />
             {trailerVideoKey && <YouTube videoId={trailerVideoKey} />}
         </Modal>
     );
