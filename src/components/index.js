554c6f06694a8b1c6914df3fcb4bcdd16c99f762
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2021-10-20 02:33:07 +0200

    :rocket: Start build the Popular Movies Slider

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,13 @@ import Footer from "./footer/Footer";
 import NowPlayingSlider from "./nowplayingslider/NowPlayingSlider";
 import PopularSlider from "./popularslider/PopularSlider";
 import TopRatedSlider from "./topratedslider/TopRatedSlider";
+import Movie from "./movie/Movie";
 
 export { 
     Navbar,
     Footer,
     NowPlayingSlider,
     PopularSlider,
-    TopRatedSlider };
+    TopRatedSlider,
+    Movie
+};
