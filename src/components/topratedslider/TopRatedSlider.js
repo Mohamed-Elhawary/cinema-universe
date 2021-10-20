18146e493725612ac92fbdbd7511e77f512393b7
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2021-10-20 02:57:42 +0200

    :rocket: update: Finish the Top Rated Slider structure

--- src/components/topratedslider/TopRatedSlider.js
+++ src/components/topratedslider/TopRatedSlider.js
@@ -1,10 +1,28 @@
+import { Col } from "react-bootstrap";
+import { checkFavorite } from "utils/helpers";
 import { Slider } from "ui";
+import { Movie } from "components";
 
 const TopRatedSlider = ({topRatedMovies}) => {
     
     return (
         <div>
             <h5>Top Rated Slider</h5>
+            <Slider>
+                {topRatedMovies.map(({id, poster_path, title, release_date, overview, vote_average}) => (
+                    <Col key={id}>
+                        <Movie 
+                            id={id}
+                            posterSrc={poster_path}
+                            title={title}
+                            date={release_date}
+                            overview={overview}
+                            rate={vote_average}
+                            isFav={checkFavorite(id)}
+                        />
+                    </Col>
+                ))}
+            </Slider>
         </div>
     );
 
