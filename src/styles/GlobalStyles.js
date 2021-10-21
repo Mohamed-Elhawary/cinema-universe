e6f7710821a0cd5518da339396e61218035ecd9d
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2021-10-21 13:12:33 +0200

    :zap: improvement: Improved the UX for the user by scrolling to the top when the user choose page from the pagination

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle} from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+    html {
+        scroll-behavior: smooth;
+    };
     body {        
         background: ${({ theme }) => theme.body_bg};
         font-family: 'Montserrat', sans-serif, Tahoma, Helvetica, Arial, Roboto;
