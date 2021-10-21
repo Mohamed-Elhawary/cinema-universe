3f35db77036ecbbd30da2d125e557d1a5b740543
Mohamed-Elhawary <mohamed.k.elhawary@gmail.com>
2021-10-21 03:43:55 +0200

    :recycle: Isolate the Sliders in the home view to separate view file

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,77 +1,13 @@
-import { useEffect, useState } from "react";
-import { useHistory } from "react-router";
-import { connect } from 'react-redux';
-import Cookies from "js-cookie";
-import { checkAuth } from "utils";
-import { useAuth } from "hooks";
-import { receiveFavorites } from "actions";
-import { fetchingHomeData } from "services";
-import { NowPlayingSlider, PopularSlider, TopRatedSlider, RecentRatedSlider, MovieModal } from "components";
+import SlidersView from "./slidersview/SlidersView";
 
-const Home = ({ movieModalOpen, setFavorites }) => {
-    
-    const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
-
-    const [popularMovies, setPopularMovies] = useState([]);
-
-    const [topRatedMovies, setTopRatedMovies] = useState([]);
-
-    const [recentRatedMovies, setRecentRatedMovies] = useState([]);
-
-    let auth = useAuth();
-
-    let history = useHistory();
-
-    useEffect(() => {
-
-        if(checkAuth()) {
-
-            fetchingHomeData(response => {
-
-                if(response.statusCode === 200) {
-    
-                    setNowPlayingMovies(response.data.nowPlaying);
-                
-                    setPopularMovies(response.data.popular);
-                
-                    setTopRatedMovies(response.data.topRated);
-    
-                    setRecentRatedMovies(response.data.recentRated);
-    
-                }
-    
-            });
-
-        } else {
-
-            auth.logout(() => history.replace("/login"));
-            
-        } 
-
-    }, []); // eslint-disable-line
-
-    useEffect(() => {
-
-        let storedFavorites = Cookies.get("favorites");
-
-        if(storedFavorites) setFavorites(JSON.parse(storedFavorites));
-        
-    }, []); // eslint-disable-line
+const Home = () => {
 
     return (
         <div className="home-view view">
-            <NowPlayingSlider nowPlayingMovies={nowPlayingMovies} />
-            <PopularSlider popularMovies={popularMovies} />
-            <TopRatedSlider topRatedMovies={topRatedMovies} />
-            <RecentRatedSlider recentRatedMovies={recentRatedMovies} />
-            {movieModalOpen && <MovieModal />}
+            <SlidersView />
         </div>
     );
 
 }
 
-const mapStateToProps = ({ movieModal }) => ({movieModalOpen: movieModal.open});
-  
-const mapDispatchToProps = dispatch => ({setFavorites: favorites => dispatch(receiveFavorites(favorites))});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
